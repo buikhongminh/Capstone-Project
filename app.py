3c61676004485f72bd7554b728c3ac9cca2dd0b2
Khong Minh <buitranthienviet@gmail.com>
2023-06-05 18:35:16 +0700

    fix lint fail

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():this
+def index():
     return render_template('index.html', title='Udacity', name='Viet Bui')
 
 app.run(host='0.0.0.0', port=80)
